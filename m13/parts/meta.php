ee865b374d4e203cf6dfb9fe3aa50fca1d76dbda
elainekim0217 <79066699+elainekim0217@users.noreply.github.com>
2021-05-24 03:36:28 +0900

    Add files via upload

---
+++ m13/parts/meta.php
@@ -0,0 +1,12 @@
+
+<meta name="viewport" content="width=device-width">
+
+<base href="http://mioweb-test.com/aau/wnm608/catanzaro.michael/m13/">
+
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="css/storetheme.css">
+
+<script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
+<script src="lib/js/functions.js"></script>
+<script src="js/templates.js"></script>
